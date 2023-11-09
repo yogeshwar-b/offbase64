5387cd5a7d0c620d755d8366c98339ff68c40215
yogeshwar-b <byogeshwarreddy@gmail.com>
2023-11-09 17:48:29 +0530

    Added Copy floating button on the text area.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'OffBase64',
       theme: ThemeData(
-        colorScheme: const ColorScheme.dark(),
+        // colorScheme: const ColorScheme.dark(),
         useMaterial3: true,
       ),
       home: Scaffold(
@@ -42,18 +43,19 @@ class MainPage extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       // crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Expanded(child: DataBox(
+        DataBox(
         htext: "Encode Text here",
           key: Key('Encode'),
            txtcontrolr: encodetxtcontroller,
           desttxtcontrolr: decodetxtcontroller,
-      )),
-        Expanded(child: DataBox(
+      ),
+        DataBox(
           htext: "Decode Text here",
           key: Key('Decode'),
             txtcontrolr: decodetxtcontroller,
           desttxtcontrolr: encodetxtcontroller,
-      ))],
+      )
+      ],
     );
   }
 }
@@ -118,6 +120,7 @@ class _DataBoxState extends State<DataBox> {
     //TODO stack copy and paste buttons on the right bottom.
     return Container(
       padding: const EdgeInsets.all(30),
+
       alignment: Alignment.center,
       child: Material(
         borderRadius: const BorderRadius.all(Radius.circular(30.0)),
@@ -134,110 +137,51 @@ class _DataBoxState extends State<DataBox> {
               widget.txtcontrolr.removeListener(updatetext);
             }
           },
-          child: TextField(
-            controller: widget.txtcontrolr,
-            keyboardType: TextInputType.multiline,
-            maxLines: 6,
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(
-                borderSide: BorderSide.none,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-              ),
-              hintText: textdata,
-              filled: true,
-              // fillColor: Colors.green,
+          child: SizedBox(
+            child: Stack(
+              children: [
+                TextField(
+                          controller: widget.txtcontrolr,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 6,
+                          decoration: InputDecoration(
+                            isDense: true,
+                            contentPadding: const EdgeInsets.fromLTRB(8, 75, 8, 8),
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                            ),
+                            hintText: textdata,
+                            filled: true,
+                            // fillColor: Colors.green,
+                          ),
+                        ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+
+                    child: Opacity(
+                      opacity: 0.7,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          await Clipboard.setData(ClipboardData(text: widget.txtcontrolr.text));
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                            content: Text("Text copied"),
+                            duration: const Duration(milliseconds: 500),
+                          ));
+                          // copied successfully
+                        },
+                        child: const Text('Copy'),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
